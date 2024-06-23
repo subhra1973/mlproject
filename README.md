36e375b0e8f8b23c4b93411e136f0cfc7806a061
Subhra Sarkar <subhra1973@gmail.com>
2024-06-24 00:14:05 +0530

    looging and exception

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## E2E project for ML 
 # update today 
+## Create virtual environment in foder with python 3.8 
+conda create -p venv python==3.8 -y
+
+## Activate env 
+
+(base) C:\project\mlproject>conda activate venv/
+
+(C:\project\mlproject\venv) C:\project\mlproject>
---
+++ src/logger.py
@@ -0,0 +1,30 @@
+import logging
+import os
+from datetime import datetime
+
+# Generate log file name based on current date and time
+LOG_FILE = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+
+# Complete logging setup
+logs_path = os.path.join(os.getcwd(), "logs")
+print(f"Logs path: {logs_path}")  # Debug print
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+print(f"Log file path: {LOG_FILE_PATH}")  # Debug print
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    logging.info("LOG file test")
+    print(f"Log file created: {LOG_FILE_PATH}")  # Confirm log file creation
+
+  
+    
+    
+
+
+
